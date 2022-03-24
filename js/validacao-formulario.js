48b03553b43e36faea79cf03b704d999f393c83c
Lucas Donha <97292308+ludonha@users.noreply.github.com>
2022-03-24 19:40:12 -0300

    :racehorse: add style-formulario

---
+++ js/validacao-formulario.js
@@ -0,0 +1,25 @@
+var botaoEnviar = document.querySelector('[data-form-botao]');
+
+var formulario = document.getElementById('formulario');
+
+const validaFormulario = () => {
+    //evento.preventDefault();
+
+    if ( formulario.nome.value.trim() === "") {
+        console.log("Campo 'nome' não pode estar em branco.")
+    }else if ( formulario.email.value.indexOf(".") == -1 ) {
+        console.log("Campo 'e-mail' deve ter '.com' no final.")
+    }else if ( formulario.assunto.value.trim() === "") {
+        console.log("Campo 'Assunto' não pode estar em branco.")
+    }else if ( formulario.mensagem.value.trim() === "") {
+        console.log("Campo 'Mensagem' não pode estar em branco.")
+    }
+
+
+}
+
+botaoEnviar.addEventListener('click', () =>{
+    validaFormulario()
+})
+
+
