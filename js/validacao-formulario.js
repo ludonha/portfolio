d8eb4d88982da116162f2812c15dfc628e219a75
Lucas Donha <97292308+ludonha@users.noreply.github.com>
2022-03-24 21:46:08 -0300

    :art: adicinado active e hover em classe lista :racehorse: adicionado icon no html

--- js/validacao-formulario.js
+++ js/validacao-formulario.js
@@ -3,7 +3,6 @@ var botaoEnviar = document.querySelector('[data-form-botao]');
 var formulario = document.getElementById('formulario');
 
 const validaFormulario = () => {
-    //evento.preventDefault();
 
     if ( formulario.nome.value.trim() === "") {
         console.log("Campo 'nome' não pode estar em branco.")
@@ -13,13 +12,15 @@ const validaFormulario = () => {
         console.log("Campo 'Assunto' não pode estar em branco.")
     }else if ( formulario.mensagem.value.trim() === "") {
         console.log("Campo 'Mensagem' não pode estar em branco.")
+    }else {
+        alert("Formulário enviado com sucesso!")
     }
 
 
 }
 
 botaoEnviar.addEventListener('click', () =>{
+    
     validaFormulario()
+
 })
-
-
